06334283a4b8427bf17f1987b7322635bf6b1ccd
rlmaster61123810 <61123810@g.cmru.ac.th>
2021-12-13 10:59:14 +0700

    adminite

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new \App\Models\User();
+        $user->user_type_id = 1;
+        $user->name = "ท่านศาสดา";
+        $user->email = "mistidea9@gmail.com";
+        $user->password = bcrypt('123456');
+        $user->username = "admin";
+        $user->save();
+    }
+}
